a1bd94f05b157869a5fb88cb5db3527878ce268b
ysfcsryo <yusufcaesaryo@gmail.com>
2025-12-09 23:07:52 +0800

    Update footer links and copyright year

--- footer.php
+++ footer.php
@@ -7,10 +7,13 @@
                     <a href="https://www.instagram.com/yuuuu.li_/" target="_blank">@yuuu.li</a> | 
                     <a href="https://www.instagram.com/divafiscalia/" target="_blank">@divafiscalia</a> | 
                     <a href="https://www.instagram.com/naapz_18/" target="_blank">@naapz_18</a>
+                    <a href="https://www.instagram.com/aryo_caesar/" target="_blank">@aryo_caesar</a>
+                    <a href="https://www.instagram.com/kii.ibrhm/" target="_blank">@kii.ibrhm</a>
+                    <a href="https://www.instagram.com/fatur_pohontu/" target="_blank">@fatur_pohontu</a>  
                 </p>
             </div>
         </div>
         <div class="text-center p-3 bg-secondary text-white">
-            © 2024 Hak Cipta Dilindungi
+            Copyright © 2025 KELOMPOK-2-TOS
         </div>
     </footer>
